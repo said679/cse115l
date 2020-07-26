b6e83bf783ad7ea0289f54db59e439fe215285ad
said679 <65491390+said679@users.noreply.github.com>
2020-07-27 01:35:13 +0600

    Lab 3 practice problem

---
+++ lab-3 problem/1 problem.c
@@ -0,0 +1,26 @@
+//Name : MD.Saidul Islam Tanveer
+//ID:1611556043
+ #include <stdio.h>
+ int main()
+ {
+   int x;
+   printf("enter the number:");
+   scanf("%d",&x);
+   if(x>=0){
+   printf("it is positive number");
+   }
+   else{
+   printf("it is negetive number");
+   }
+   return 0;
+
+
+
+   }
+
+
+
+
+
+
+
---
+++ lab-3 problem/3 problem.c
@@ -0,0 +1,21 @@
+//Name:Md.Saidul islam tanveer
+//Id:1611556043
+
+#include<stdio.h>
+int main(){
+int x;
+printf("enter the number:");
+scanf("%d",&x);
+ if(x%2==0){
+  printf("number is of even");
+ }
+ else
+ printf("number is odd");
+
+ return 0;
+
+
+
+
+
+}
