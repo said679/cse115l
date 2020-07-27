59759a87cc9fcffa1f17ad98b8b82b8c06dcc573
said679 <65491390+said679@users.noreply.github.com>
2020-07-27 23:32:40 +0600

    class excercise lab-4 er

---
+++ class excercise lab-4/exercise-3.c
@@ -0,0 +1,22 @@
+//Name:mad.Saidul Islam Tanveer
+//Id:1611556043
+
+#include<stdio.h>
+int main()
+{
+ char choice;
+ printf("Enter a character:");
+ scanf("%c",&choice);
+ switch(choice)
+ {
+ case 'a':
+ printf("You typed a");
+ break;
+ case 'b':
+ printf("You typed b");
+ break;
+ default:
+ printf("You typed something else");
+ }
+ return 0;
+}
